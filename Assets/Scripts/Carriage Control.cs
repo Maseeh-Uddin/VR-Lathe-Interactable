d013a5d003392f8658fc624b08d17bfe12a1acdb
Maseeh-Uddin <maseehuddin777@gmail.com>
2024-02-15 19:05:39 +0530

    Feed Wheels are now interactable

--- Assets/Scripts/Carriage Control.cs
+++ Assets/Scripts/Carriage Control.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class CarriageControl : MonoBehaviour
 {
+    GameObject feedWheel1;
+    private Quaternion initialRotation;
+    private float translationSpeed = 0.04f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        feedWheel1 = GameObject.Find("handle1");
+        initialRotation = feedWheel1.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D)) { transform.Translate(0.005f, 0, 0); }
-        if (Input.GetKey(KeyCode.A)) { transform.Translate(-0.0014f, 0, 0); }
+        // Calculate the rotation change
+        Quaternion rotationChange = feedWheel1.transform.rotation * Quaternion.Inverse(initialRotation);
 
+        // Convert the rotation change to Euler angles for translation
+        Vector3 eulerRotationChange = rotationChange.eulerAngles;
+        // Use the change in rotation to adjust translation
+        float translationAmount = eulerRotationChange.y * translationSpeed;
+        if (eulerRotationChange.x > 330)
+        {
+            // Update the position based on the calculated translation
+            transform.Translate(translationAmount,0, 0);
+            Debug.Log("near full " + eulerRotationChange);
+
+        }
+        else
+        {
+            transform.Translate(-translationAmount,0, 0);
+            Debug.Log("near zero " + eulerRotationChange);
+
+        }
+
+
+        // Update the initial rotation for the next frame
+        initialRotation = feedWheel1.transform.rotation;
     }
 }
--- Assets/Scripts/Tool Post Control.cs
+++ Assets/Scripts/Tool Post Control.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class ToolPostControl : MonoBehaviour
 {
+    GameObject feedWheel2;
+    private Quaternion initialRotation;
+    private float translationSpeed = 0.02f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        feedWheel2 = GameObject.Find("handle2");
+        initialRotation = feedWheel2.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) { transform.Translate(0, 0, 0.005f); }
-        if (Input.GetKey(KeyCode.S)) { transform.Translate(0, 0, -0.005f); }
+        // Calculate the rotation change
+        Quaternion rotationChange = feedWheel2.transform.rotation * Quaternion.Inverse(initialRotation);
+
+        // Convert the rotation change to Euler angles for translation
+        Vector3 eulerRotationChange = rotationChange.eulerAngles;
+        // Use the change in rotation to adjust translation
+        float translationAmount = eulerRotationChange.y * translationSpeed;
+        if (eulerRotationChange.x > 330){
+            // Update the position based on the calculated translation
+            transform.Translate(0, 0, translationAmount);
+            Debug.Log("near full "+eulerRotationChange);
+
+        }
+        else
+        {
+            transform.Translate(0, 0, -translationAmount);
+            Debug.Log("near zero "+eulerRotationChange);
+
+        }
+
+
+        // Update the initial rotation for the next frame
+        initialRotation = feedWheel2.transform.rotation;
     }
 }
